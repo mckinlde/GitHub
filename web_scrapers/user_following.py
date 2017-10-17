362fc2372ff4a1db51639d202e6acec2f6711a44
mckinled <douglas.e.mckinley@vanderbilt.edu>
2017-10-17 15:34:50 -0700

    user_following populates a list of all the usernames a user follows

---
+++ web_scrapers/user_following.py
@@ -0,0 +1,36 @@
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import re
+
+#Let's get a list of all the users a user follows
+
+#URL will be of format: https://github.com/ USERNAME ?tab=following
+BASE_URL = "https://github.com/"
+URL_END = "?tab=following"
+
+
+def retrieve(url: str):
+    """retrieves content at the specified url"""
+    print("*", url)
+    sleep(1)  # Play nice with GitHub's Bandwidth
+    r = requests.get(url, verify=False)  # get the HTML; ignore SSL errors (present on this particular site)
+    soup = BeautifulSoup(r.text, "lxml")  # parse the HTML
+    return soup
+
+def get_followed_usernames(username: str):
+    #gather all the usernames a user follows
+
+    result_list = [] # we'll store them in a list
+
+    url = BASE_URL + username + URL_END
+    soup = retrieve(url)
+    #as of 10/17/2017 all this info is held in link-gray pl-1 tags
+    usernames = soup.findAll("span", "link-gray pl-1")
+    for username in usernames:
+        result_list.append(username.text)
+    return result_list
+
+
+print(get_followed_usernames('mckinlde'))
